20820cd74d124bcfa3492f5e1b6ea75ecc2d93ab
inhamostova <inha.mostova@gmail.com>
2026-07-03 17:30:27 +0300

    first task fixes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,33 +1,35 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 
-let idInt = null;
+let timerId = null;
 
-startBtn.addEventListener('click', onStartClcik);
+startBtn.addEventListener('click', onStartClick);
 stopBtn.addEventListener('click', onStopClick);
 
 setDisabledAttribute(stopBtn);
 
 function onStopClick(evt) {
-  clearInterval(idInt);
+  clearInterval(timerId);
   setDisabledAttribute(evt.target);
   removeDisabledAttribute(startBtn);
 }
 
-function onStartClcik(evt) {
-  idInt = setInterval(() => {
-    document.body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
+function onStartClick(evt) {
+  timerId = setInterval(changeBackgroundColor, 1000);
   setDisabledAttribute(evt.target);
   removeDisabledAttribute(stopBtn);
 }
 
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
 function setDisabledAttribute(btn) {
-  btn.setAttribute('disabled', 'true');
+  btn.disabled = true;
 }
 
 function removeDisabledAttribute(btn) {
-  btn.removeAttribute('disabled');
+  btn.disabled = false;
 }
 
 function getRandomHexColor() {
