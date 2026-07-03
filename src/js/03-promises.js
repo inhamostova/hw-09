07d7ce3e13e0596036294bfd5dea3e064b5b8c66
inhamostova <inha.mostova@gmail.com>
2026-07-03 23:25:26 +0300

    learn promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,11 +7,44 @@ function createPromise(position, delay) {
   }
 }
 
-const a = 5;
-const b = 10;
+// const a = 5;
+// const b = 10;
 
-function sum(x, y) {
-  return x + y;
-}
+// function sum(x, y) {
+//   return x + y;
+// }
 
-console.log(sum(a, b));
+// console.log(sum(a, b));
+
+// const makeGreeting = guestName => {
+//   if (guestName === '' || guestName === undefined) {
+//     return {
+//       success: false,
+//       message: 'Guest name must not be empty',
+//     };
+//   }
+
+//   return {
+//     success: true,
+//     message: `Welcome ${guestName}`,
+//   };
+// };
+
+// const result = makeGreeting('Mango');
+
+// if (result.success) {
+//   console.log(result.message);
+// } else {
+//   console.error(result.message);
+// }
+
+const makeGreeting = guestName => {
+  if (guestName === '' || guestName === undefined) {
+    return Promise.reject('Guest name must not be empty');
+  }
+  return new Promise(resolve => resolve(`Welcome ${guestName}`));
+};
+
+const result = makeGreeting('');
+
+result.then(value => console.log(value)).catch(error => console.log(error));
