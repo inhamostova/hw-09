a5f27b6f942d4d88b86739aac871091dfac1c9dc
inhamostova <inha.mostova@gmail.com>
2026-07-05 21:01:36 +0000

    Deploying to gh-pages from @ inhamostova/hw-09@6814b7fad21da8ec4808ac06dbbb45ca948023c8 🚀

--- 03-promises.28c25ddf.js
+++
@@ -1,2 +0,0 @@
-var e;(""===(e="")||void 0===e?Promise.reject("Guest name must not be empty"):new Promise((o=>o(`Welcome ${e}`)))).then((e=>console.log(e))).catch((e=>console.log(e)));
-//# sourceMappingURL=03-promises.28c25ddf.js.map
--- 03-promises.29e62378.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;(""===(n="")||void 0===n?Promise.reject("Guest name must not be empty"):new Promise((function(e){return e("Welcome ".concat(n))}))).then((function(n){return console.log(n)})).catch((function(n){return console.log(n)}))}();
-//# sourceMappingURL=03-promises.29e62378.js.map
