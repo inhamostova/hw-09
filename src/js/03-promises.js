ba2c52a8e3c828ceaecf7968ef2f0ff99547c130
inhamostova <inha.mostova@gmail.com>
2026-07-06 21:30:58 +0300

    third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,79 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  let delay = Number(evt.target.elements.delay.value);
+  const step = Number(evt.target.elements.step.value);
+  const amount = Number(evt.target.elements.amount.value);
+
+  evt.target.reset();
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
 }
 
-// const promise = new Promise((resolve, reject) => {
-//   const canFulfill = Math.random() > 0.5;
-//   setTimeout(() => {
-//     if (canFulfill) {
-//       resolve('fulfill');
-//     }
-
-//     reject('promise run with error');
-//   }, 2000);
-// });
-
-// console.log(promise);
-// promise.then(
-//   res => console.log(`✅ ${res}`),
-//   err => console.log(`❌ ${err}`)
-// );
-
-// const makeOrder = (dish, onSuccess, onError) => {
-//   const passed = Math.random() > 0.5;
-
-//   setTimeout(() => {
-//     if (passed) {
-//       onSuccess(`Here is your dish ${dish}`);
-//     } else {
-//       onError('Finish dishes ❤️‍🩹');
-//     }
-//   }, 1000);
-// };
-
-const makeOrder = dish => {
-  return new Promise((resolve, reject) => {
-    const passed = Math.random() > 0.5;
-
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((res, rej) => {
     setTimeout(() => {
-      if (passed) {
-        resolve(`Here is your dish ${dish}`);
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
       }
-      reject('Finish dishes ❤️‍🩹');
-    }, 1000);
+    }, delay);
   });
-};
-
-makeOrder('piro*ok')
-  .then(onOrderSuccess)
-  .catch(onOrderError)
-  .finally(() => console.log('block finally'));
-
-// makeOrder('piro*ok', onOrderSuccess, onOrderError);
-
-function onOrderSuccess(res) {
-  console.log('onOrderSuccess');
-  console.log(res);
 }
-
-function onOrderError(err) {
-  console.log('onOrderError❌');
-  console.log(err);
-}
-
-// makeOrder('piro*ok', onOrderSuccess, onOrderError);
-
-// function onOrderSuccess(res) {
-//   console.log('onOrderSuccess');
-//   console.log(res);
-// }
-
-// function onOrderError(err) {
-//   console.log('onOrderError❌');
-//   console.log(err);
-// }
