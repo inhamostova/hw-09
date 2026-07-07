dd16ed3aa75deb80b9cb66774ea5b4ec0b6b6799
inhamostova <inha.mostova@gmail.com>
2026-07-07 17:07:19 +0300

    change 4 task with method allSettled

--- src/js/04-loto.js
+++ src/js/04-loto.js
@@ -6,45 +6,65 @@ const container = document.querySelector('.js-container');
 btnStart.addEventListener('click', onStart);
 
 function onStart() {
-  const result = [];
+  let counter = 0;
   const boxArray = [...container.children];
+  const promises = boxArray.map(createPromise);
+  Promise.allSettled(promises).then(items => {
+    items.forEach((item, i) => {
+      boxArray[i].textContent = '';
+      setTimeout(() => {
+        boxArray[i].textContent = item.value || item.reason;
+        if (item.status === 'fulfilled') counter += 1;
 
-  boxArray.forEach(box => insertTextContent(box, ''));
-  boxArray.forEach((box, i) => {
-    createPromise(i)
-      .then(val => {
-        insertTextContent(box, val);
-        result.push(val);
-      })
-      .catch(val => insertTextContent(box, val))
-      .finally(() => {
         if (i === boxArray.length - 1) {
-          setTimeout(() => {
-            if (!result.length || result.length === boxArray.length) {
-              Notify.success('Winner');
-            } else {
-              Notify.failure('You lose money');
-            }
-          }, 500);
+          if (counter === boxArray.length || !counter) {
+            Notify.success('Winner');
+          } else {
+            Notify.failure('Looser!');
+          }
         }
-      });
+      }, 1000 * i);
+    });
   });
 }
 
-function createPromise(delay) {
+function createPromise() {
   return new Promise((res, rej) => {
     const isSuccess = Math.random() > 0.5;
-
-    setTimeout(() => {
-      if (isSuccess) {
-        res('🍋');
-      } else {
-        rej('❌');
-      }
-    }, 1000 * delay);
+    if (isSuccess) {
+      res('🍋');
+    } else {
+      rej('❌');
+    }
   });
 }
 
-function insertTextContent(box, text) {
-  box.textContent = text;
-}
+// function onStart() {
+//   const result = [];
+//   const boxArray = [...container.children];
+
+//   boxArray.forEach(box => insertTextContent(box, ''));
+//   boxArray.forEach((box, i) => {
+//     createPromise(i)
+//       .then(val => {
+//         insertTextContent(box, val);
+//         result.push(val);
+//       })
+//       .catch(val => insertTextContent(box, val))
+//       .finally(() => {
+//         if (i === boxArray.length - 1) {
+//           setTimeout(() => {
+//             if (!result.length || result.length === boxArray.length) {
+//               Notify.success('Winner');
+//             } else {
+//               Notify.failure('You lose money');
+//             }
+//           }, 500);
+//         }
+//       });
+//   });
+// }
+
+// function insertTextContent(box, text) {
+//   box.textContent = text;
+// }
